dfe9ad5d32ca7022d770d596c5830bf79c4792ea
MiguelP <j.patrocinio.staiy@gmail.com>
2021-01-29 12:40:29 +0100

    Added icons to the drawer navigator

--- navigation/ShopNavigation.js
+++ navigation/ShopNavigation.js
@@ -1,8 +1,10 @@
 // Third-party imports
+import React from "react";
 import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import { createDrawerNavigator } from "react-navigation-drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 // Global imports
 import ProductOverviewScreen from "../screens/shop/ProductOverviewScreen";
@@ -38,6 +40,15 @@ const ProdcuctsNavigator = createStackNavigator(
     Cart: CartScreen,
   },
   {
+    navigationOptions: {
+      drawerIcon: (drawerConfig) => (
+        <Ionicons
+          name={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
+    },
     defaultNavigationOptions: defaultStackNavOptions,
   }
 );
@@ -49,6 +60,15 @@ const OrdersNavigation = createStackNavigator(
     Orders: OrdersScreen,
   },
   {
+    navigationOptions: {
+      drawerIcon: (drawerConfig) => (
+        <Ionicons
+          name={Platform.OS === "android" ? "md-list" : "ios-list"}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
+    },
     defaultNavigationOptions: defaultStackNavOptions,
   }
 );
